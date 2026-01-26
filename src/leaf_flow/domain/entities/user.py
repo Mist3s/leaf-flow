87eeddaa409d7daed94795fea32db2fbd292b023
Ivanov Andrey <ivanov.andrevv@gmail.com>
2026-01-26 21:03:17 +0200

    fix: UserEntity

--- src/leaf_flow/domain/entities/user.py
+++ src/leaf_flow/domain/entities/user.py
@@ -6,9 +6,9 @@ class UserEntity:
     id: int
     first_name: str
     telegram_id: int | None = None
-    email: str = None
-    last_name: str = None
-    username: str = None
-    language_code: str = None
-    photo_url: str = None
-    password_hash: str = None
+    email: str | None = None
+    last_name: str | None = None
+    username: str | None = None
+    language_code: str | None = None
+    photo_url: str | None = None
+    password_hash: str | None = None
